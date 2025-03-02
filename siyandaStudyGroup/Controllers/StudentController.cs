e11a5b56dd6b34930fd536b5bfeb2883ebcab2d6
nyama <nyama@LAPTOP-8VJNNTCL>
2025-03-02 21:02:46 +0200

    Add project files.

---
+++ siyandaStudyGroup/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace siyandaStudyGroup.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult ListStudent()
+        {
+            List<Models.StudentModel> student = new List<Models.StudentModel>();
+            student.Add(new Models.StudentModel { StudentNumber = "u24765644", Name = "Siyanda", Surname = "Twala", EmailAddress = "siyanda.twala@tuks.co.za" });
+            student.Add(new Models.StudentModel { StudentNumber = "u24765747", Name = "Aiden", Surname = "Brooks", EmailAddress = "aiden.brooks@tuks.co.za" });
+            student.Add(new Models.StudentModel { StudentNumber = "u24765699", Name = "Noah", Surname = "Evans", EmailAddress = "noah.evans@tuks.co.za" });
+            student.Add(new Models.StudentModel { StudentNumber = "u24765740", Name = "Tshepho", Surname = "Ledwaba", EmailAddress = "tshepho.ledwaba@tuks.co.za" });
+            student.Add(new Models.StudentModel { StudentNumber = "u24765546", Name = "Lucas", Surname = "Gray", EmailAddress = "lucas.gray@tuks.co.za" });
+            return View(student);
+        }
+    }
+}
---
+++ siyandaStudyGroup/Models/StudentModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace siyandaStudyGroup.Models
+{
+    public class StudentModel
+    {
+        [Display(Name = "Student Number")]
+        public string StudentNumber { get; set; }
+
+
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Surname")]
+        public string Surname { get; set; }
+
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+
+    }
+}
